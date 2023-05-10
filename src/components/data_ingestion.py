1d38bbcc5076730704aa049c64aeccdcb5b40364
Ahmad Bader <56409106+Ahmadbader17@users.noreply.github.com>
2023-05-10 11:17:53 +0530

    worked on further data related things

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,22 +1,20 @@
-from dataclasses import dataclass
-from email import header
-from operator import index
 import os
 import sys
 from src.logger import logging
 from src.exception import CustomException
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
-## initialize the Data Ingestion Configuration
+## intialize the data ingestion configuration
+
 @dataclass
 class DataIngestionconfig:
-    train_data_path: str=os.path.join('artifacts','train.csv')
-    test_data_path: str=os.path.join('artifacts','test.csv')
-    raw_data_path: str=os.path.join('artifacts','raw.csv')
+    train_data_path=os.path.join('artifacts','train.csv')
+    test_data_path=os.path.join('artifacts','test.csv')
+    raw_data_path=os.path.join('artifacts','raw.csv')
+
 
 ## create a data ingestion class
 class DataIngestion:
@@ -31,11 +29,12 @@ class DataIngestion:
             logging.info('Dataset read as pandas Dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+
             df.to_csv(self.ingestion_config.raw_data_path,index=False)
 
-            logging.info('Raw data is created')
-
+            logging.info("Train test split")
             train_set,test_set=train_test_split(df,test_size=0.30,random_state=42)
+
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
@@ -44,8 +43,17 @@ class DataIngestion:
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
+
             )
 
+
+
         except Exception as e:
-            logging.info('Exception occured at Data Ingestion stage')
-            raise CustomException(e,sys)
+            logging.info('Error occured in Data Ingestion config')
+
+
+
+
+
+
+
---
+++ src/pipelines/training_pipeline.py
@@ -0,0 +1,15 @@
+from cgi import test
+from inspect import ismethoddescriptor
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+
+from src.components.data_ingestion import DataIngestion
+
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    print(train_data_path,test_data_path)
+    
